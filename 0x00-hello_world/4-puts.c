1b8a4f85f66c62e32a99faa2733e770280c9eced
Madu Jang <madu.jang93@gmail.com>
2023-02-16 17:19:11 -0800

    using puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,10 @@
 
 int main(void)
 {
+	//string initialisation
 	char myStr[] = "\"Programming is like building a multilingual puzzle";
+
+	//writing the string to stdout
 	puts(myStr);
 
 	return (0);

a110dca56c56558d0be4b2536865aeead7c53f56
Madu Jang <madu.jang93@gmail.com>
2023-02-21 18:38:00 -0800

    print first 98 fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,44 +8,36 @@
 int main(void)
 {
 	int i;
-	unsigned long fib1 = 0, fib2 = 1, sum;
-	unsigned long fib1_h1, fib1_h2, fib2_h1, fib2_h2;
-	unsigned long h1, h2;
+	unsigned long f1 = 0, f2 = 1, s, f1h1, f1h2, f2h1, f2h2, h1, h2;
 
 	for (i = 0; i < 92; i++)
 	{
-		sum = fib1 + fib2;
-		printf("%lu, ", sum);
-
-		fib1 = fib2;
-		fib2 = sum;
+		s = f1 + f2;
+		printf("%lu, ", s);
+		f1 = f2;
+		f2 = s;
 	}
-
-	fib1_h1 = fib1 / 10000000000;
-	fib2_h1 = fib2 / 10000000000;
-	fib1_h2 = fib1 % 10000000000;
-	fib2_h2 = fib2 % 10000000000;
-
+	f1h1 = f1 / 10000000000;
+	f2h1 = f2 / 10000000000;
+	f1h2 = f1 % 10000000000;
+	f2h2 = f2 % 10000000000;
 	for (i = 93; i < 99; i++)
 	{
-		h1 = fib1_h1 + fib2_h1;
-		h2 = fib1_h2 + fib2_h2;
-		if (fib1_h2 + fib2_h2 > 9999999999)
+		h1 = f1h1 + f2h1;
+		h2 = f1h2 + f2h2;
+		if (f1h2 + f2h2 > 9999999999)
 		{
 			h1 += 1;
 			h2 %= 10000000000;
 		}
-
 		printf("%lu%lu", h1, h2);
 		if (i != 98)
 			printf(", ");
-
-		fib1_h1 = fib2_h1;
-		fib1_h2 = fib2_h2;
-		fib2_h1 = h1;
-		fib2_h2 = h2;
+		f1h1 = f2h1;
+		f1h2 = f2h2;
+		f2h1 = h1;
+		f2h2 = h2;
 	}
 	printf("\n");
-
 	return (0);
 }

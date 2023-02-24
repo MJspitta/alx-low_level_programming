b1fb32a05bb8b49c9e7ed75a6df48656bbaf3c83
Madu Jang <madu.jang93@gmail.com>
2023-02-23 16:29:28 -0800

    add two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiply 2 integers
+ * @a: input integer
+ * @b: input integer
+ * Return: result
+ */
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+
+	return (result);
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,2 +1,3 @@
 0. Check for upper case character
 1. Check for a digit 0 through 9
+2. Multiply two integers

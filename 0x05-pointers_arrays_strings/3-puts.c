3b60102691edfc4c36a9c3ded2d8a1b856920cb1
Madu Jang <madu.jang93@gmail.com>
2023-02-27 14:50:17 -0800

    print string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - print a string
+ * @str: character string
+ * Return: nothing
+ */
+void _puts(char *str)
+{
+	int i;
+
+	i = 0;
+	while(*(str + i) != '\0')
+	{
+		_putchar(*(str + i));
+		i++;
+	}
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -1,3 +1,4 @@
 0. Update the value it points to to 98
 1. Swap value of two integers
 2. Return length of a string
+3. Print a string

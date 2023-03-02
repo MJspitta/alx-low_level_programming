1a81901466b23f932c316091e73edb59aa2eb9e0
Madu Jang <madu.jang93@gmail.com>
2023-03-02 03:02:03 -0800

    compare 2 strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,16 +10,13 @@ int _strcmp(char *s1, char *s2)
 {
 	int i, j;
 
-	i = 0;
-	j = 0;
+	i = j = 0;
 	while (*(s1 + i) != '\0')
 		i++;
 	while (*(s2 + j) != '\0')
 		j++;
-	if (i < j)
-		return (-15);
-	else if (i > j)
-		return (15);
-	else
+	if(i == j)
 		return (0);
+	else
+		return (i - j);
 }

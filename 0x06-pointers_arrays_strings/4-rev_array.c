2f9ae4539530fc93875a59762b8c1a4c95f156de
Madu Jang <madu.jang93@gmail.com>
2023-03-02 04:12:38 -0800

    reverse array of integers

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverse an array of integers
+ * @a: array of integers
+ * @n: number of elements
+ * Return: nothing
+ */
+void reverse_array(int *a, int n)
+{
+	int temp, start, end;
+
+	start = 0;
+	end = n - 1;
+	while (start < end)
+	{
+		temp = *(a + start);
+		*(a + start) = *(a + end);
+		*(a + end) = temp;
+		start++;
+		end--;
+	}
+}
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -2,3 +2,4 @@
 1. A function that concatenates two strings using at most n bytes from src
 2. Function that copies a string
 3. Function that compares two strings
+4. Function that reverses the content of a array of integers

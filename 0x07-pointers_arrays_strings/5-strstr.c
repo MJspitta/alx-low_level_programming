f07c576f2099c094abb1aa3379270ef334bae64c
Madu Jang <madu.jang93@gmail.com>
2023-03-06 16:07:45 -0800

    5-strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -12,7 +12,9 @@ char *_strstr(char *haystack, char *needle)
 	int i, j;
 
 	if (needle[0] == '\0')
+	{
 		return haystack;
+	}
 
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
@@ -21,12 +23,16 @@ char *_strstr(char *haystack, char *needle)
 			for (j = 0; needle[j] != '\0'; j++)
 			{
 				if (haystack[i + j] != needle[j])
+				{
 					break;
+				}
 			}
 			if (needle[j] == '\0')
+			{
 				return (haystack + i);
+			}
 		}
 	}
-	
+
 	return ('\0');
 }

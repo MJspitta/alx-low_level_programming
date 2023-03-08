0b258be4facca9a5113216ce0ff91ae455d87cb4
Madu Jang <madu.jang93@gmail.com>
2023-03-08 15:30:34 -0800

    x to pow y

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - value of x to pow y
+ * @x: number
+ * @y: power
+ *
+ * Return: value of x raised to pow y
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	
+	if (y > 0)
+		return (x * _pow_recursion(x, y - 1));
+	return (1);
+}

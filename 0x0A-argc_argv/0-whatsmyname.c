a058eaca54806c2c1cbb081ed3de03952d19ade2
Madu Jang <madu.jang93@gmail.com>
2023-03-10 12:48:24 -0800

    0-whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Start here
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 (Success)
+ */
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}

bdae8f7611c9dc12dd0ff21457b9fb8f37771f2f
Madu Jang <madu.jang93@gmail.com>
2023-03-10 12:56:01 -0800

    1-args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Start of program
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 (Success)
+ */
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
--- 0x0A-argc_argv/README.md
+++ 0x0A-argc_argv/README.md
@@ -1,1 +1,2 @@
 0. Program that prints its name
+1. Program that prints the number of arguments passed into it

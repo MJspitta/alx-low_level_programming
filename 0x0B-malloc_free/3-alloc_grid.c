a31c43a74ecf5374799fb4d21a4aaa897436eeb1
Madu Jang <madu.jang93@gmail.com>
2023-03-14 11:06:36 -0700

    alloc grid

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * alloc_grid - 2 dimensional array of integers
+ * @width: width of array
+ * @height: height of array
+ *
+ * Return: grid (Success) NULL (Failure)
+ */
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **ar;
+
+	if (height <= 0 || width <= 0)
+		return (NULL);
+
+	ar = (int **) malloc(sizeof(int *) * height);
+	if (ar == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		ar[i] = (int *) malloc(sizeof(int) * width);
+		if (ar[i] == NULL)
+		{
+			free(ar);
+
+			for (j = 0; j <= i; j++)
+				free(ar[j]);
+
+			return (NULL);
+		}
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			ar[i][j] = 0;
+		}
+	}
+
+	return(ar);
+}
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -1,3 +1,4 @@
 0. Function that creates an array of chars and initializes it with a specific char
 1. Function that returns a pointer to a newly allocated space in memory
 2. Function that concatenates two strings
+3. Function that returns a pointer to a 2 dimensional array of integers

61b585fc4456d9e14b49b6805928e30c295219a5
Madu Jang <madu.jang93@gmail.com>
2023-03-22 14:11:26 -0700

    perform simple operations

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,67 @@
+#include "3-calc.h"
+
+/**
+ * op_add - sum of two integers
+ * @a: first int
+ * @b: second int
+ *
+ * Return: int sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - difference of two integers
+ * @a: first int
+ * @b: second int
+ *
+ * Return: int difference
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - product of two integers
+ * @a: first int
+ * @b: second int
+ *
+ * Return: int product
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - division of two integers
+ * @a: first int
+ * @b: second int
+ *
+ * Return: int division
+ */
+int op_div(int a, int b)
+{
+	if (b != 0)
+		return (a / b);
+	printf("Error\n");
+	exit(100);
+}
+
+/**
+ * op_mod - remainder of a division of two numbers
+ * @a: first int
+ * @b: second int
+ *
+ * Return: int remainder
+ */
+int op_mod(int a, int b)
+{
+	if (b != 0)
+		return (a % b);
+	printf("Error\n");
+	exit(100);
+}

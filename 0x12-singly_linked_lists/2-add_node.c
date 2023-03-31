571d7e90653be260e728d80085b2f93f12cd11ef
Madu Jang <madu.jang93@gmail.com>
2023-03-30 17:50:21 -0700

    add node

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,49 @@
+#include "lists.h"
+
+unsigned int get_len(const char *str);
+
+/**
+ * add_node - add node at beginning of list
+ * @head: first node pointer
+ * @str: string pointer
+ *
+ * Return: pointer to list
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new = (list_t *) malloc(sizeof(list_t));
+	unsigned int len;
+
+	if (new == NULL)
+		return (NULL);
+	
+	len = get_len(str);
+	new->str = strdup(str);
+	new->len = len;
+	new->next = (*head);
+	(*head) = new;
+
+	return (*head);
+}
+
+/**
+ * get_len - length of string
+ * @str: string
+ *
+ * Return: length of string
+ */
+unsigned int get_len(const char *str)
+{
+	unsigned int i, len = 0;
+
+	if (str == NULL)
+		return (0);
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		len++;
+		i++;
+	}
+	return (len);
+}
--- 0x12-singly_linked_lists/README.md
+++ 0x12-singly_linked_lists/README.md
@@ -1,2 +1,3 @@
 0. Function that prints all elements of a list
 1. Function that returns the number of elements in a linked list
+2. Fiunction that adds a new node at the beginning of a list

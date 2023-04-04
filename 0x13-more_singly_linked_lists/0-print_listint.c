1fc915fa312f1a9faedbacabd9aed56f85d77c71
Madu Jang <madu.jang93@gmail.com>
2023-04-04 03:29:41 -0700

    first commit

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_listint - print all elements of a list
+ * @h: pointer to node
+ *
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	int i;
+
+	i = 0;
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		i++;
+		h = h->next;
+	}
+
+	return (i);
+}

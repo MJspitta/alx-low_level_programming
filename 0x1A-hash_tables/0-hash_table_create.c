232fdd038489d0b89ef8c9a410ae373f8002ee04
Madu Jang <madu.jang93@gmail.com>
2023-07-07 03:04:04 -0700

    parenthesis on return statement

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,13 +13,13 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	table = (hash_table_t *)malloc(sizeof(hash_table_t));
 	if (table == NULL)
-		return NULL;
+		return (NULL);
 
 	table->array = (hash_node_t **)malloc(size * sizeof(hash_node_t *));
 	if (table->array == NULL)
 	{
 		free(table);
-		return NULL;
+		return (NULL);
 	}
 
 	for (i = 0; i < size; i++)
@@ -27,5 +27,5 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	table->size = size;
 
-	return table;
+	return (table);
 }
